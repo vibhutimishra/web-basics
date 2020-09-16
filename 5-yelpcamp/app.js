d35742fa5f17dc4fd6324624579f2587e3012139
vibhutimishra <vibhuti.mishra09@gmail.com>
2020-09-17 02:43:03 +0530

    yelpcamp

---
+++ 5-yelpcamp/app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var app=express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+
+var camp=[
+    {name:"salmon creet",image:"https://www.photosforclass.com/download/px_4268158"},
+    {name:"Granite Hill", image:"https://www.photosforclass.com/download/pb_2512944"},
+];
+
+app.get("/",function(req,res){
+    res.render("landing.ejs");
+});
+
+app.get("/campgrounds",function(req,res){
+     res.render("campground",{campgrounds:camp});
+});
+
+app.get("/campgrounds/form",function(req,res){
+    res.render("form.ejs")
+});
+app.post("/campgrounds",function(req,res){
+     var name=req.body.name;
+     var url=req.body.image;
+     var b={name:name, image:url}
+     camp.push(b);
+     res.redirect("/campgrounds");
+});
+
+app.listen("3000",function(req,res){
+ console.log("SERVER STARTED");
+});
