93a49ee651d88bc4a5ebda387cccb48fe3c57598
vibhutimishra <vibhuti.mishra09@gmail.com>
2020-09-30 14:50:26 +0530

    bog-site added

---
+++ 6-blog-website/app.js
@@ -0,0 +1,25 @@
+const express= require("express");
+var app= express();
+var mongoose = require("mongoose");
+const bodyParser = require("body-parser")
+
+
+mongoose.connect("mongodb://localhost/restful-blog-app")
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+var blogSchema = new mongoose.Schema({
+    title:String,
+    image:String,
+    body:String,
+    created: { type:Date, default:Date.now }
+});
+var blog = mongoose.model("Blog",blogSchema);
+
+# Restful-routes
+app.get("/")
+
+
+app.listen("3000",function(req,res){
+    console.log("Server started");
+});
