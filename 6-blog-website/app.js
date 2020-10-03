b4aafdb7304efa27088b76b511037abe3ff5b6fa
vibhutimishra <vibhuti.mishra09@gmail.com>
2020-10-04 01:18:23 +0530

    blog created

--- 6-blog-website/app.js
+++ 6-blog-website/app.js
@@ -2,11 +2,13 @@ const express= require("express");
 var app= express();
 var mongoose = require("mongoose");
 var ejs = require('ejs');
-const bodyParser = require("body-parser")
+var path=require("path");
+const bodyParser = require("body-parser");
+const { request } = require("http");
 
 
 mongoose.connect("mongodb://localhost/mydb")
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine', 'ejs');
@@ -34,6 +36,25 @@ app.get("/blogs",function(req,res){
 });
 app.get("/blogs/new",function(req,res){
     res.render("new.ejs");
+});
+
+app.post("/blogs",function(req,res){
+    var title = req.body.title;
+    var image = req.body.image;
+    var about = req.body.body;
+    var b = {
+        title: title,
+        image: image,
+        body:about
+    };
+    blog.create(b,function(err,newblog){
+        if(err){
+            res.redirect("/blogs/new");
+        }
+        else{
+            res.redirect("/blogs");
+        }
+    });
 });
 
 app.listen("3000",function(req,res){
